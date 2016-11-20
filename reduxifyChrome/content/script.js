90465833cbfcad1c6c75f2ed0f352de288d08235
Stephenie Reimer <steferu@gmail.com>
2016-11-20 14:02:00 -0800

    Add files via upload

---
+++ reduxifyChrome/content/script.js
@@ -0,0 +1,8 @@
+var headingText = document.getElementsByTagName("div");
+
+for(let i = 0; i < headingText.length; i++){
+	headingText[i].style.color = "#ff0000";
+}
+
+
+//try it w/ this site: http://www.color-hex.com/color/0000ff
