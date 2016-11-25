f630fe2973c42832d5fa7e1fe715e2d91d8cc149
mlampedx <mlampesd@gmail.com>
2016-11-25 14:11:24 -0800

    committing last change

--- Library/src/reduxifyExtractor.js
+++ Library/src/reduxifyExtractor.js
@@ -20,6 +20,35 @@ reduxify.ReducerExtractor = (reducers) => {
   return structuredReducerObj;
 }
 
+//repl.it
+
+// function abc () { switch(def) { case 'ACTION': break; case 'REACTION': break;  } }
+// function def () { switch(abc) { case 'INACTION': break; } }
+
+// var testReducers = (reducers) => {
+//   let reduxifyObj = {};
+//   let coerceToStr = '';
+//     for (let k in reducers) {
+//       reduxifyObj[k] = reducers[k] + coerceToStr;
+//     }
+//   let returnVal = JSON.stringify(reduxifyObj);
+//  // console.log(returnVal)
+//   var structuredReducers = {};
+//    var reducersKeys = returnVal.match(/"\w*":"/gi).map(key => key.replace(/["':]/gi, ''));
+//       var reducersArr = returnVal.split(',');
+//    reducersKeys.forEach((key, i) => structuredReducers[key] = reducersArr[i]);
+//   console.log(structuredReducers)
+  
+// }
+
+// console.log(testReducers({abc, def}))
+
+
+//    //var reducerCases = reducersArr.map(reducer => reducer.match(/case\s.*':/gi));
+//   // var reducerCases2 = reducerCases.map(c => c.replace(/[case\\':]/gi, ''));
+//  // const reducersKeys2 = reducersKeys.map(key => key.replace(/["':]/gi, '').slice(0, -8))
+
+
 /**
  * 
  *  Parses actionCreators function definitions for type properties on returned payloads. 
