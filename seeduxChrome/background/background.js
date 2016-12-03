d43961618b4fd7e4a5f67d6ff19c84dea26cb7db
srei <steferu@gmail.com>
2016-12-03 11:04:22 -0800

    finalized comments on log-->--script-->background logic

--- seeduxChrome/background/background.js
+++ seeduxChrome/background/background.js
@@ -1,7 +1,8 @@
 /**
- * Listens for events in the seedux lifecycle. Triggered when a message is sent from the content script or from the application; specifically in order to either: (a) log a story entry, (b) retrieve the current log, (c) clear the log's history, (d) undo an action from the log, (e) redo an action from the log, or (f) store the application's tab ID, which is used at a later point to implement the undo and redo messages.
+ * Listens for events in the seedux lifecycle. Triggered when a message is sent from the content script or from the application. Will either: (a) log a story entry, (b) retrieve the current log, (c) clear the log's history, (d) undo an action, (e) redo an action, or (f) store the application's tab ID, which is used at a later point to implement the undo and redo messages.
  *
- * @type {String}
+ * @param {Object} msg is an object sent by the calling script. Contains multiple type properties.
+ *
  */
 
 let history = [];
--- seeduxChrome/content/script.js
+++ seeduxChrome/content/script.js
@@ -4,7 +4,7 @@
 
 
  /**
- * Listens for  custom DOM event 'actionDispatched' and sends a message to the background script of seeDux with a new historyEntry object which is attached to the e.detail property.
+ * Listens for custom DOM event 'actionDispatched' and sends a message obj to the background script of seeDux with a new historyEntry and a type of 'addToLog'.
  */
 document.addEventListener('actionDispatched', function(e){
 	// console.log('I heard an action! Sending to background...', e.detail);
@@ -15,7 +15,7 @@ document.addEventListener('actionDispatched', function(e){
 }, false);
 
  /**
- * Listens for  custom DOM event 'codeParsed' which is dispatched from combineReducers function and sends a message to the background script of seeDux with a parsed-code object which is attached to the e.detail property.
+ * Listens for  custom DOM event 'codeParsed' which is dispatched from the combineReducers function. Sends a message object to the background script of seeDux with parsed-code and a type of 'storeCode'.
  */
 document.addEventListener('codeParsed', function(e){
 	// console.log('Code Parsing event heard! Sending to background...', e.detail);
@@ -26,7 +26,7 @@ document.addEventListener('codeParsed', function(e){
 }, false);
 
  /**
- * Listens for any messages received from Seedux's background script and sent to seedux middleware via the DOM.
+ * Listens for any messages sent via the DOM and received from Seedux's background script. Initializes en event based on the message type and dispatches event.
  */
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
 	// console.log('Got a message! ', msg);
@@ -37,7 +37,7 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
 });
 
  /**
- * Creates a custom event whose value is initialized and then dispatched to the seedux Extractor in order to parse information.
+ * Creates a custom event 'scriptLoaded' which is then dispatched to the seedux extractor so that the extractor can parse information.
  */
 var evt = document.createEvent('Event');
 evt.initEvent('scriptLoaded', true, true);
