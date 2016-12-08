6894af7b1ba3573a287d343066f7c0ba3173833d
Ahmad K <lance@codemosey.net>
2016-12-07 19:39:44 -0800

    comments out all console.logs that face tool user

--- seeduxChrome/content/script.js
+++ seeduxChrome/content/script.js
@@ -9,7 +9,7 @@
  * to the background script of seeDux with a new historyEntry and a type of 'addToLog'.
  */
 document.addEventListener('actionDispatched', (e) => {
-	// console.log('I heard an action! Sending to background...', e.detail);
+	// // console.log('I heard an action! Sending to background...', e.detail);
   const msg = {};
   msg.historyEntry = e.detail;
   msg.type = 'addToLog';
@@ -22,7 +22,7 @@ document.addEventListener('actionDispatched', (e) => {
  * and a type of 'storeCode'.
  */
 document.addEventListener('codeParsed', (e) => {
-	// console.log('Code Parsing event heard! Sending to background...', e.detail);
+	// // console.log('Code Parsing event heard! Sending to background...', e.detail);
   const msg = {};
   msg.codeObj = e.detail;
   msg.type = 'storeCode';
@@ -34,10 +34,10 @@ document.addEventListener('codeParsed', (e) => {
  * and forwards them on to our middleware.
  */
 chrome.runtime.onMessage.addListener((msg) => {
-	console.log('Got a message! Will forward to library code', msg);
+	// console.log('Got a message! Will forward to library code', msg);
   const evt = document.createEvent('CustomEvent');
   evt.initCustomEvent(msg.type, true, true, msg.action);
-  console.log('CONTENT_SCRIPT: Dispatching event: ', evt);
+  // console.log('CONTENT_SCRIPT: Dispatching event: ', evt);
   document.dispatchEvent(evt);
 });
 
@@ -47,5 +47,5 @@ chrome.runtime.onMessage.addListener((msg) => {
  */
 const scriptLoadedEvt = document.createEvent('Event');
 scriptLoadedEvt.initEvent('scriptLoaded', true, true);
-console.log('CONTENT_SCRIPT: Dispatching event: ', scriptLoadedEvt);
+// console.log('CONTENT_SCRIPT: Dispatching event: ', scriptLoadedEvt);
 document.dispatchEvent(scriptLoadedEvt);
