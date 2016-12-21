e05338dbb5983a26c35684a86bae73d473fa2e89
mlampedx <mlampesd@gmail.com>
2016-12-20 16:57:30 -0800

    Removed unnecessary console.logs

--- seeduxChrome/src/components/D3Viz.jsx
+++ seeduxChrome/src/components/D3Viz.jsx
@@ -10,8 +10,7 @@ class D3Viz extends Component {
   render() {
     const { data, chartType, zoomLevel, d3Table, searchTerms, recentFilter } = this.props;
 
-    //--- D3 LOGIC -----
-    // The canvas for the tree
+    // Helper function that manipulates a faux DOM node to create the D3 visualization
     const fauxNode = ReactFauxDom.createElement('div');
     transformVizNode(fauxNode, data, chartType, zoomLevel, d3Table, searchTerms, recentFilter);
     return fauxNode.toReact();
